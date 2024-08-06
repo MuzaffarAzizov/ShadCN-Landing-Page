34d40ed1d5f5492fcfef2a6b0d85c162c63ad6fb
MuzaffarAzizov <muzaffar2903@gmail.com>
2024-08-06 11:55:51 -0500

    Add more features

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "blue-violet": "rb",
+        blackColor: "#0C0A09",
+        mycolor: "#66656A",
+        greenColor: "#22C55E",
+        grayColor: "#191817",
+        whiteColor: "#F2F2F2",
+        lightColor: "#636667",
+        color: "#3db30e",
+        darkGreen: "#1D3B25",
       },
     },
   },
